1a7edd23092b571006b4cb351d2a6bdaf9a944f0
Radwanul Islam Nayeem <rnayeem221523@bscse.uiu.ac.bd>
2024-09-19 08:51:02 +0600

    Revert "added colors and fonts in tailwind.config.js"

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
 ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#ff4c30",
-        secondary: "#161616",
-      },
-      fontFamily: {
-        merriweather: '"Merriweather", serif',
-        nunito: '"Nunito Sans", sans-serif'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
