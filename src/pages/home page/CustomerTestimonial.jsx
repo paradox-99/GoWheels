581b8c89e82abfba8712ee9abd9109a991b04bc9
Bithe <farzana.hossain5@gmail.com>
2024-09-19 16:36:45 +0600

    testimonial done

--- src/pages/home page/CustomerTestimonial.jsx
+++ src/pages/home page/CustomerTestimonial.jsx
@@ -1,29 +1,37 @@
 // import { useQuery } from "@tanstack/react-query";
 // import axios from "axios";
 import { CiStar } from "react-icons/ci";
+import BasicHeading from "../../../src/components/BasicHeading";
 
 const CustomerTestimonial = () => {
-
-    // const {data: testimonials =[], isLoading, error} = useQuery({
-    //     queryKey: ["testimonial"],
-    //     queryFn: async ()=>{
-    //         const response = await axios.get("/testimonial");
-    //         console.log(response.data);
-    //         return response.data;
-    //     }
-    // });
-    // if (isLoading) return <div>Loading surveys...</div>;
-    // if (error) return <div>Error loading surveys</div>;
-
+  // const {data: testimonials =[], isLoading, error} = useQuery({
+  //     queryKey: ["testimonial"],
+  //     queryFn: async ()=>{
+  //         const response = await axios.get("/testimonial");
+  //         console.log(response.data);
+  //         return response.data;
+  //     }
+  // });
+  // if (isLoading) return <div>Loading surveys...</div>;
+  // if (error) return <div>Error loading surveys</div>;
 
   return (
-    <div>
+    <div className="max-w-6xl mx-auto mt-[200px] px-6 pb-20">
+      <BasicHeading
+        title="TESTIMONIAL"
+        heading={"What Our Client Say About Us"}
+        desc="Don’t just take our word for it—hear what our satisfied customers have to say about their rental experiences with us!"
+      ></BasicHeading>{" "}
       <div className="max-w-screen-xl mx-auto p-16">
         <div className="sm:grid lg:grid-cols-3 sm:grid-cols-2 gap-10">
           <div className="hover:bg-[#ff4c30] hover:text-white transition duration-300 max-w-sm rounded-3xl overflow-hidden shadow-lg">
             <div className="py-8 px-8">
               <div className="flex items-center justify-between">
-                <CiStar />
+                <div className="flex">
+                  <CiStar />
+                  <CiStar />
+                  <CiStar />
+                </div>
                 <div className="flex items-center gap-2">
                   <img
                     src="https://tailwindcss.com/img/jonathan.jpg"
@@ -31,21 +39,64 @@ const CustomerTestimonial = () => {
                     alt="Profile"
                   />
                   <h1 className="font-bold">Name</h1>
+                </div>
+              </div>
+              <a href="#">                <h4 className="text-sm mb-3 font-semibold">Booked :  2/3/2023 - 12-12-2023 </h4>
+              </a>
+              <p className="mb-2 text-sm">
+              I needed a last-minute rental, and [Your Car Rental Company] delivered! The rates were affordable, and the car was in excellent shape. Their customer service team was very helpful as well.
+              </p>
+            </div>
+          </div>
+          <div className="hover:bg-[#ff4c30] hover:text-white transition duration-300 max-w-sm rounded-3xl overflow-hidden shadow-lg">
+            <div className="py-8 px-8">
+              <div className="flex items-center justify-between">
+                <div className="flex">
+                  <CiStar />
+                  <CiStar />
+                  <CiStar />
+                </div>
+                <div className="flex items-center gap-2">
+                  <img
+                    src="https://tailwindcss.com/img/jonathan.jpg"
+                    className="rounded-full h-12 w-12 mb-4"
+                    alt="Profile"
+                  />
                   <h1 className="font-bold">Name</h1>
                 </div>
               </div>
-              <a href="#">
-                <h4 className="text-lg mb-3 font-semibold">Booked: </h4>
+              <a href="#">                <h4 className="text-sm mb-3 font-semibold">Booked :  2/3/2023 - 12-12-2023 </h4>
               </a>
               <p className="mb-2 text-sm">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industrys standard dummy text
-                ever since the 1500s
+              I needed a last-minute rental, and [Your Car Rental Company] delivered! The rates were affordable, and the car was in excellent shape. Their customer service team was very helpful as well.
               </p>
-              
-           
             </div>
           </div>
+          <div className="hover:bg-[#ff4c30] hover:text-white transition duration-300 max-w-sm rounded-3xl overflow-hidden shadow-lg">
+            <div className="py-8 px-8">
+              <div className="flex items-center justify-between">
+                <div className="flex">
+                  <CiStar />
+                  <CiStar />
+                  <CiStar />
+                </div>
+                <div className="flex items-center gap-2">
+                  <img
+                    src="https://tailwindcss.com/img/jonathan.jpg"
+                    className="rounded-full h-12 w-12 mb-4"
+                    alt="Profile"
+                  />
+                  <h1 className="font-bold">Name</h1>
+                </div>
+              </div>
+              <a href="#">                <h4 className="text-sm mb-3 font-semibold">Booked :  2/3/2023 - 12-12-2023 </h4>
+              </a>
+              <p className="mb-2 text-sm">
+              I needed a last-minute rental, and [Your Car Rental Company] delivered! The rates were affordable, and the car was in excellent shape. Their customer service team was very helpful as well.
+              </p>
+            </div>
+          </div>
+         
         </div>
       </div>
     </div>
--- src/pages/home page/HomePage.jsx
+++ src/pages/home page/HomePage.jsx
@@ -1,18 +1,9 @@
 import Banner from "./Banner";
-<<<<<<< HEAD
-import CustomerTestimonial from "./CustomerTestimonial";
 
-const HomePage = () => {
-  return (
-    <div>
-      <h1>Hello Dev Dynamos</h1>
-      <Banner></Banner>{" "}
-      <CustomerTestimonial></CustomerTestimonial>
-    </div>
-  );
-=======
+
 import HowItWorks from "../../components/HowItWorks";
 import WhyChooseUs from "../../components/WhyChooseUs";
+import CustomerTestimonial from "./CustomerTestimonial";
 
 const HomePage = () => {
     return (
@@ -21,9 +12,9 @@ const HomePage = () => {
             <HowItWorks></HowItWorks>
             <WhyChooseUs></WhyChooseUs>
             <HowItWorks></HowItWorks>
+            <CustomerTestimonial></CustomerTestimonial>
         </div>
     );
->>>>>>> 0f5e982c6b0d149483daaed68d9eb51274aa78b1
 };
 
 export default HomePage;
