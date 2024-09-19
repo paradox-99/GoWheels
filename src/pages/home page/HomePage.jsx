05453f1856c82f8683199bef364c0e1531d102b7
mk-munna <mkmunnaofficial@gmail.com>
2024-09-19 14:58:40 +0600

    why choose us section added

--- src/pages/home page/HomePage.jsx
+++ src/pages/home page/HomePage.jsx
@@ -1,10 +1,12 @@
 import HowItWorks from "../../components/HowItWorks";
+import WhyChooseUs from "../../components/WhyChooseUs";
 
 const HomePage = () => {
     return (
         <div>
             <h1>Hello Dev Dynamos</h1>
             <HowItWorks></HowItWorks>
+            <WhyChooseUs></WhyChooseUs>
         </div>
     );
 };
