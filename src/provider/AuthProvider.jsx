6680969c99920def8099ab1d447c1f2d5761d037
Masum Billah <masumbillah2062003@gmail.com>
2024-09-24 15:25:19 +0600

    test

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,19 +1,14 @@
-import { Children, createContext } from "react";
-export const AuthContext = createContext(null)
+import { createContext, useState } from "react";
+export const AuthContext = createContext(null);
 
+const AuthProvider = ({ Children }) => {
+  const [user, setUser] = useState(null);
+  const [loader, setLoader] = useState(true);
 
-const AuthProvider = () => {
-   
-    const [user, setUser] = useState(null);
-    const [loader, setLoader] = useState(true);
-
-
-    const authInfo = { user, setUser, loader, setLoader}
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {Children}
-        </AuthContext.Provider>
-    );
+  const authInfo = { user, setUser, loader, setLoader };
+  return (
+    <AuthContext.Provider value={authInfo}>{Children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
