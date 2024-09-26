e1879f70ab4dda8233c0ca0f22838f6ab358340b
Radwanul Islam Nayeem <rnayeem221523@bscse.uiu.ac.bd>
2024-09-26 16:37:56 +0600

    Update firebase.config.js

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCJNcZIvssBlfXVoE2sY41z6mrN79yuakM",
-    authDomain: "gowheels-99.firebaseapp.com",
-    projectId: "gowheels-99",
-    storageBucket: "gowheels-99.appspot.com",
-    messagingSenderId: "882993135704",
-    appId: "1:882993135704:web:79eb95486dcd5628c6ca2d",
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 const app = initializeApp(firebaseConfig);
