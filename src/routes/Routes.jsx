b8b1ddd8b2a6336c5f199ac32fb89aa6f15f9b91
Masum Billah <masumbillah2062003@gmail.com>
2024-09-26 14:20:17 +0600

    added search route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -10,56 +10,61 @@ import SignUpPartFour from "../pages/signup/SignUpPartFour";
 import SignUpPartFive from "../pages/signup/SignUpPartFive";
 import SignupPartTwo from "../pages/signup/SignupPartTwo";
 import SignUpLastPage from "../pages/signup/SignUpLastPage";
+import Filter from "../pages/Filter/Filter";
 
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
         path: "/",
-        element: <Root></Root>,
+        element: <HomePage></HomePage>,
+      },
+      {
+        path: "/about",
+        element: <AboutPage></AboutPage>,
+      },
+      {
+        path: "/contact",
+        element: <Contactpage></Contactpage>,
+      },
+      {
+        path: "/filter",
+        element: <Filter></Filter>,
+      },
+      {
+        path: "/join",
+        element: <Background></Background>,
         children: [
-            {
-                path: "/",
-                element: <HomePage></HomePage>
-            },
-            {
-                path: "/about",
-                element: <AboutPage></AboutPage>
-            },
-            {
-                path: "/contact",
-                element: <Contactpage></Contactpage>
-            },
-            {
-                path: '/join',
-                element: <Background></Background>,
-                children: [
-                    {
-                        path: '/join',
-                        element: <SignIn></SignIn>
-                    },
-                    {
-                        path: "/join/register-new",
-                        element: <SignUp></SignUp>
-                    },
-                    {
-                        path: '/join/signUpTwo',
-                        element: <SignupPartTwo></SignupPartTwo>
-                    },
-                    {
-                        path: '/join/signUpFour',
-                        element: <SignUpPartFour></SignUpPartFour>
-                    },
-                    {
-                        path: '/join/signUpLastPage',
-                        element: <SignUpLastPage></SignUpLastPage>
-                    },
-                    {
-                        path: '/join/signUpFive',
-                        element: <SignUpPartFive></SignUpPartFive>,
-                    }
-                ]
-            },
-        ]
-    },
+          {
+            path: "/join",
+            element: <SignIn></SignIn>,
+          },
+          {
+            path: "/join/register-new",
+            element: <SignUp></SignUp>,
+          },
+          {
+            path: "/join/signUpTwo",
+            element: <SignupPartTwo></SignupPartTwo>,
+          },
+          {
+            path: "/join/signUpFour",
+            element: <SignUpPartFour></SignUpPartFour>,
+          },
+          {
+            path: "/join/signUpLastPage",
+            element: <SignUpLastPage></SignUpLastPage>,
+          },
+          {
+            path: "/join/signUpFive",
+            element: <SignUpPartFive></SignUpPartFive>,
+          },
+        ],
+      },
+    ],
+  },
 ]);
 
 export default router;
