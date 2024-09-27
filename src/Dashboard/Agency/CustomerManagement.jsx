7e7460b12c470adbe89c3c11f218ec9b5775ace6
Bithe <farzana.hossain5@gmail.com>
2024-09-27 13:50:36 +0600

    customer management route

--- src/Dashboard/Agency/CustomerManagement.jsx
+++ src/Dashboard/Agency/CustomerManagement.jsx
@@ -1,10 +1,129 @@
 
 const CustomerManagement = () => {
     return (
-        <div>
-            this is customer management
-        </div>
-    );
+<div className=" mx-auto">
+          <div className="block mb-4 mx-auto border-b border-slate-300 pb-2 max-w-[360px]">
+            <a
+              target="_blank"
+              href=""
+              className="block w-full px-4 py-2 text-center text-slate-700 transition-all "
+            >
+              Manage Your All <b>Rental Cars</b>.
+            </a>
+          </div>
+  
+          <div className="w-full flex justify-between items-center mb-3 mt-1 pl-3">
+            <div>
+              <h3 className="text-lg font-semibold text-slate-800">
+                Overview of the Booking Cars
+              </h3>
+             
+            </div>
+            <div className="ml-3">
+              <div className="w-full relative">
+                <div className="relative">
+                  <input
+                    className="bg-white w-full pr-11 h-10 pl-3 py-2 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded transition duration-200 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
+                    placeholder="Search"
+                  />
+                  <button
+                    className="absolute h-8 w-8 right-1 top-1 my-auto px-2 flex items-center bg-white rounded "
+                    type="button"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      className="w-8 h-8 text-slate-600"
+                    >
+                      <path d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+  
+          <div className="relative flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-lg bg-clip-border">
+            <table className="w-full text-left table-auto min-w-max">
+              <thead>
+                <tr>
+                  <th className="p-4 border-b border-slate-200 bg-slate-50">
+                    <p className="text-sm font-normal leading-none text-slate-500">
+                      License Number
+                    </p>
+                  </th>
+                  <th className="p-4 border-b border-slate-200 bg-slate-50">
+                    <p className="text-sm font-normal leading-none text-slate-500">
+                      Customer Name
+                    </p>
+                  </th>
+                  <th className="p-4 border-b border-slate-200 bg-slate-50">
+                    <p className="text-sm font-normal leading-none text-slate-500">
+                      Booking Date & Time
+                    </p>
+                  </th>
+                  <th className="p-4 border-b border-slate-200 bg-slate-50">
+                    <p className="text-sm font-normal leading-none text-slate-500">
+                      Return Date & Time
+                    </p>
+                  </th>
+                  <th className="p-4 border-b border-slate-200 bg-slate-50">
+                    <p className="text-sm font-normal leading-none text-slate-500">
+                      Rental Amount
+                    </p>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="hover:bg-slate-50 border-b border-slate-200">
+                  <td className="p-4 py-5">
+                    <p className="block font-semibold text-sm text-slate-800">
+                      L998979978
+                    </p>
+                  </td>
+                  <td className="p-4 py-5">
+                    <p className="text-sm text-slate-500">farzana</p>
+                  </td>
+                  <td className="p-4 py-5">
+                    <p className="text-sm text-slate-500">29th April, 2024</p>
+                  </td>
+                  <td className="p-4 py-5">
+                    <p className="text-sm text-slate-500">28th September, 2024</p>
+                  </td>
+                  <td className="p-4 py-5">
+                    <p className="text-sm text-slate-500">1500</p>
+                  </td>
+                </tr>
+               
+              </tbody>
+            </table>
+  
+            <div className="flex justify-between items-center px-4 py-3">
+              <div className="text-sm text-slate-500">
+                Showing <b>1-5</b> of 45
+              </div>
+              <div className="flex space-x-1">
+                <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+                  Prev
+                </button>
+                <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-white bg-slate-800 border border-slate-800 rounded hover:bg-slate-600 hover:border-slate-600 transition duration-200 ease">
+                  1
+                </button>
+                <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+                  2
+                </button>
+                <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+                  3
+                </button>
+                <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+                  Next
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>    );
 };
 
 export default CustomerManagement;
