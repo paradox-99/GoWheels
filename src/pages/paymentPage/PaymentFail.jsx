f9eef5604b2d1c28a67ebc452ee749dc724282f5
Masum Billah <masumbillah2062003@gmail.com>
2024-10-10 07:13:10 +0600

    added payment success page

--- src/pages/paymentPage/PaymentFail.jsx
+++ src/pages/paymentPage/PaymentFail.jsx
@@ -1,9 +1,21 @@
-
+import { Link } from "react-router-dom";
+import fail from "/paymentfail.png"
 
 const PaymentFail = () => {
     return (
-        <div>
-            <h1>Payment Fail</h1>
+        <div className="flex justify-center items-center">
+            {/* <h1>Payment Success : {tranId}</h1> */}
+            <div className="max-w-screen-sm pb-8 border border-gray-300 flex justify-start items-center flex-col px-10">
+                <figure>
+                    <img src={fail} alt="" className="w-52 " />
+                </figure>
+                <h2 className="text-2xl text-gray-500">Payment Fail</h2>
+                <div className="flex justify-center items-center flex-col pt-10 gap-5">
+                    <Link to="/" className="bg-primary hover:bg-transparent hover:border-2 border-primary hover:text-primary duration-500 active:scale-75 shadow-inner shadow-secondary border-2 px-6 py-2 text-background font-nunito font-semibold">
+                        Try Again
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/paymentPage/PaymentSuccess.jsx
+++ src/pages/paymentPage/PaymentSuccess.jsx
@@ -1,12 +1,25 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import success from "/paymentsuccess.png"
 
 const PaymentSuccess = () => {
     const { tranId } = useParams()
     return (
-        <div >
-            <h1>Payment Success : {tranId}</h1>
-            <div>
+        <div className="flex justify-center items-center">
+            {/* <h1>Payment Success : {tranId}</h1> */}
+            <div className="max-w-screen-sm pb-8 border border-gray-300 flex justify-start items-center flex-col px-10">
+                <figure>
+                    <img src={success} alt="" className="w-60 " />
+                </figure>
+                <h2 className="text-2xl text-gray-500">Payment Successful</h2>
+                <div className="flex justify-center items-center flex-col pt-10 gap-5">
+                    <Link to="/" className="bg-primary hover:bg-transparent hover:border-2 border-primary hover:text-primary duration-500 active:scale-75 shadow-inner shadow-secondary border-2 px-6 py-2 text-background font-nunito font-semibold">
+                       Go Back Homepage
+                    </Link>
+                    <button className="hover:bg-primary bg-transparent hover:border-2 border-primary text-primary duration-500 active:scale-75 shadow-inner shadow-secondary border-2  px-6 py-2 hover:text-background font-nunito font-semibold">
+                        View Order Details
+                    </button>
 
+                </div>
             </div>
         </div>
     );
